2cd4d52028252b8cbab417834d9511850be127e9
yazeedb94 <ybaniissa92@gmail.com>
2025-11-07 22:50:29 +0300

    la update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",
+  base: "/NexaBot/",
   server: {
     host: "::",
     port: 8080,
